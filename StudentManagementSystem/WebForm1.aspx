108fb0663b3d97688ad7e71757b9f29395298483
sabbirziauddin <sabbir07bsc@gmail.com>
2015-10-27 12:08:28 +0600

    Initial commit

---
+++ StudentManagementSystem/WebForm1.aspx
@@ -0,0 +1,128 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="StudentManagementSystem.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label1" runat="server" Font-Bold="True" Font-Size="Larger" ForeColor="Red" Text="Student Management System"></asp:Label>
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Student Id:"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="IdTextBox" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label10" runat="server" Text="Search By Dept:"></asp:Label>
+&nbsp;&nbsp;
+        <asp:TextBox ID="searchTextBox" runat="server" OnTextChanged="searchTextBox_TextChanged"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="Student Name:"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="NameTextBox" runat="server"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="searchbydeptButton" runat="server" OnClick="searchbydeptButton_Click" Text="Search by DEPT:" />
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="Father Name:"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="FatherTextBox" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label5" runat="server" Text="Mother Name:"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="MotherTextBox" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label6" runat="server" Text="Address:"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="AddressTextBox" runat="server"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="showResultButton" runat="server" Font-Bold="True" OnClick="showResultButton_Click" Text="Show Studnet Result:" />
+        <br />
+        <asp:Label ID="Label7" runat="server" Text="Contact No:"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="contTextBox" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label8" runat="server" Text="Email:"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="emailTextBox" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label9" runat="server" Text="Instittution:"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="deptTextBox" runat="server"></asp:TextBox>
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="msgLabel" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:Button ID="addButton" runat="server" OnClick="addButton_Click" Text="Add Student" />
+&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="delateButton" runat="server" Text="Delete Student" OnClick="delateButton_Click" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="updateButton" runat="server" Text="Update Student" OnClick="updateButton_Click" />
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="showallButton" runat="server" Font-Bold="True" OnClick="Button1_Click" Text="Show all:" />
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button1" runat="server" Font-Bold="True" OnClick="Button1_Click1" Text="Search by id:" />
+        &nbsp;<br />
+        <br />
+        <asp:GridView ID="showGridView" runat="server" AutoGenerateColumns="False" BackColor="White" BorderColor="White" BorderStyle="Ridge" BorderWidth="2px" CellPadding="3" CellSpacing="1" GridLines="None" Height="124px" Width="862px">
+            <Columns>
+                <asp:BoundField DataField="Id" HeaderText="ID" />
+                <asp:BoundField DataField="stuname" HeaderText="studentname" />
+                <asp:BoundField DataField="fathername" HeaderText="Fathername" />
+                <asp:BoundField DataField="mothername" HeaderText="Mothername" />
+                <asp:BoundField DataField="address" HeaderText="Address" />
+                <asp:BoundField DataField="contactNo" HeaderText="ContactNo" />
+                <asp:BoundField DataField="email" HeaderText="Email" />
+                <asp:BoundField DataField="Department" HeaderText="Department" />
+            </Columns>
+            <FooterStyle BackColor="#C6C3C6" ForeColor="Black" />
+            <HeaderStyle BackColor="#4A3C8C" Font-Bold="True" ForeColor="#E7E7FF" />
+            <PagerStyle BackColor="#C6C3C6" ForeColor="Black" HorizontalAlign="Right" />
+            <RowStyle BackColor="#DEDFDE" ForeColor="Black" />
+            <SelectedRowStyle BackColor="#9471DE" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+            <SortedAscendingHeaderStyle BackColor="#594B9C" />
+            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+            <SortedDescendingHeaderStyle BackColor="#33276A" />
+        </asp:GridView>
+        <br />
+        <br />
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" BackColor="White" BorderColor="#999999" BorderStyle="Solid" BorderWidth="1px" CellPadding="3" ForeColor="Black" GridLines="Vertical" Width="846px">
+            <AlternatingRowStyle BackColor="#CCCCCC" />
+            <Columns>
+                <asp:BoundField DataField="idnumber" HeaderText="Id" />
+                <asp:BoundField DataField="name" HeaderText="Name" />
+                <asp:BoundField DataField="deppartment" HeaderText="Department" />
+                <asp:BoundField DataField="email" HeaderText="email" />
+                <asp:BoundField DataField="grade" HeaderText="Grade" />
+                <asp:BoundField DataField="cgpa" HeaderText="CGPA" />
+                <asp:BoundField DataField="courses" HeaderText="Courses" />
+                <asp:BoundField DataField="semester" HeaderText="semester" />
+            </Columns>
+            <FooterStyle BackColor="#CCCCCC" />
+            <HeaderStyle BackColor="Black" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#999999" ForeColor="Black" HorizontalAlign="Center" />
+            <SelectedRowStyle BackColor="#000099" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+            <SortedAscendingHeaderStyle BackColor="Gray" />
+            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+            <SortedDescendingHeaderStyle BackColor="#383838" />
+        </asp:GridView>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ StudentManagementSystem/WebForm1.aspx.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StudentManagementSystem
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            SMSentities dbcontext = new SMSentities();
+            studinfo stdobj = new studinfo();
+            stdobj.Id = int.Parse(IdTextBox.Text);
+            stdobj.stuname = NameTextBox.Text;
+            stdobj.fathername = FatherTextBox.Text;
+            stdobj.mothername = MotherTextBox.Text;
+            stdobj.address = AddressTextBox.Text;
+            stdobj.contactNo = int.Parse(contTextBox.Text);
+            stdobj.email = emailTextBox.Text;
+            stdobj.DepartMent = deptTextBox.Text;
+            dbcontext.studinfoes.Add(stdobj);
+            dbcontext.SaveChanges();
+            msgLabel.Text = "added successfully";
+            //SMSentities dbcontext=new SMSentities();
+            //studinfo obj =new studinfo();
+            //obj.Id = int.Parse(IdTextBox.Text);
+            //obj.stuname = NameTextBox.Text;
+            //obj.fathername = FatherTextBox.Text;
+            //obj.mothername = MotherTextBox.Text;
+            //obj.address = AddressTextBox.Text;
+            //obj.contactNo = int.Parse(contTextBox.Text);
+            //obj.email = emailTextBox.Text;
+            //obj.DepartMent = deptTextBox.Text;
+            //dbcontext.studinfoes.Add(obj);
+            //dbcontext.SaveChanges();
+
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SMSentities dbcontex=new SMSentities();
+            var studinfor = from dd in dbcontex.studinfoes select dd;
+            //var stumarks=from xx in dbcontex.studinfoes
+            DataTable dt = new DataTable();
+            dt.Columns.Add("id");
+            dt.Columns.Add("stuname");
+            dt.Columns.Add("fathername");
+            dt.Columns.Add("mothername");
+            dt.Columns.Add("address");
+            dt.Columns.Add("contactno");
+            dt.Columns.Add("email");
+            dt.Columns.Add("department");
+            foreach (var studd in studinfor)
+            {
+                DataRow r = dt.NewRow();
+                r[0] = studd.Id;
+                r[1] = studd.stuname;
+                r[2] = studd.fathername;
+                r[3] = studd.mothername;
+                r[4] = studd.address;
+                r[5] = studd.contactNo;
+                r[6] = studd.email;
+                r[7] = studd.DepartMent;
+                dt.Rows.Add(r);
+
+               
+
+
+
+            }
+            showGridView.DataSource = dt;
+            showGridView.DataBind();
+
+        }
+
+        protected void searchTextBox_TextChanged(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        protected void searchbydeptButton_Click(object sender, EventArgs e)
+        {
+            string dept = searchTextBox.Text;
+            SMSentities dbcontex = new SMSentities();
+            var studinfor = from xx in dbcontex.studinfoes where xx.DepartMent == dept select xx;
+            DataTable dt = new DataTable();
+            dt.Columns.Add("id");
+            dt.Columns.Add("stuname");
+            dt.Columns.Add("fathername");
+            dt.Columns.Add("mothername");
+            dt.Columns.Add("address");
+            dt.Columns.Add("contactno");
+            dt.Columns.Add("email");
+            dt.Columns.Add("department");
+            foreach (var studentin in studinfor)
+            {
+                DataRow r = dt.NewRow();
+                r[0] = studentin.Id;
+                r[1] = studentin.stuname;
+                r[2] = studentin.fathername;
+                r[3] = studentin.mothername;
+                r[4] = studentin.address;
+                r[5] = studentin.contactNo;
+                r[6] = studentin.email;
+                r[7] = studentin.DepartMent;
+                dt.Rows.Add(r);
+            }
+            showGridView.DataSource = dt;
+            showGridView.DataBind();
+        }
+
+        protected void updateButton_Click(object sender, EventArgs e)
+        {
+            SMSentities dbcontex=new SMSentities();
+            StudentManagementSystem.studinfo objStudinfo=new studinfo();
+            //try
+            {
+                int id1 = int.Parse(IdTextBox.Text);
+                objStudinfo = dbcontex.studinfoes.FirstOrDefault(xx => xx.Id==id1);
+                objStudinfo.DepartMent = deptTextBox.Text;
+                objStudinfo.address = AddressTextBox.Text;
+                dbcontex.SaveChanges();
+                msgLabel.Text = "update successfully";
+            }
+            //catch (Exception e1)
+            {
+                msgLabel.Text = "not found id";
+                //throw;
+            }
+
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            
+            SMSentities dbcontext=new SMSentities();
+            int id1 = int.Parse(IdTextBox.Text);
+            var studobj = from xx in dbcontext.studinfoes where xx.Id == id1 select xx;
+            bool flag = false;
+            foreach (var stdin in studobj)
+            {
+                NameTextBox.Text = stdin.stuname;
+                FatherTextBox.Text = stdin.fathername;
+                MotherTextBox.Text = stdin.mothername;
+                AddressTextBox.Text = stdin.address;
+                contTextBox.Text = stdin.contactNo.ToString();
+                emailTextBox.Text = stdin.email;
+                deptTextBox.Text = stdin.DepartMent;
+                flag = true;
+
+
+            }
+            if (flag == false)
+            {
+                msgLabel.Text = "not found";
+
+            }
+            else
+            {
+                msgLabel.Text = "found";
+            }
+
+        }
+
+        protected void delateButton_Click(object sender, EventArgs e)
+        {
+            SMSentities dbcontext=new SMSentities();
+            StudentManagementSystem.studinfo sobject = new studinfo();
+           // try
+            {
+                int id1 = int.Parse(IdTextBox.Text);
+                var sobj = dbcontext.studinfoes.FirstOrDefault(xx => xx.Id == id1);
+                dbcontext.studinfoes.Remove(sobj);
+                dbcontext.SaveChanges();
+                msgLabel.Text = "successfully delated";
+
+            }
+           // catch (Exception e1)
+            {
+
+                msgLabel.Text = "not found";
+            }
+          
+
+            {
+                
+            }
+
+
+
+        }
+
+        protected void showResultButton_Click(object sender, EventArgs e)
+        {
+            SMSentities dbcontext=new SMSentities();
+            var stumarks = from xx in dbcontext.stdmarksDetails select xx;
+            DataTable dt=new DataTable();
+            dt.Columns.Add("idnumber");
+            dt.Columns.Add("name");
+            dt.Columns.Add("deppartment");
+            dt.Columns.Add("email");
+            dt.Columns.Add("grade");
+            dt.Columns.Add("cgpa");
+            dt.Columns.Add("courses");
+            dt.Columns.Add("semester");
+            foreach (var stdma in stumarks)
+            {
+                DataRow dr = dt.NewRow();
+                dr[0] = stdma.idNumber;
+                dr[1] = stdma.name;
+                dr[2] = stdma.DeppartMent;
+                dr[3] = stdma.Email;
+                dr[4] = stdma.grade;
+                dr[5] = stdma.cgpa;
+                dr[6] = stdma.courses;
+                dr[7] = stdma.semester;
+                dt.Rows.Add(dr);
+            }
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+    }
+}
